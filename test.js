86feb0b4c9723a3bd8b6efff330c3d95faeedc3f
realprudhvi <realprudhvi@gmail.com>
2026-03-08 15:39:40 +0530

    Improved file structure

--- test.js
+++
@@ -1,9 +0,0 @@
-const { GoogleGenAI } = require('@google/genai');
-
-async function test() {
-    const ai = new GoogleGenAI({ apiKey: "" });
-    const models = await ai.models.list();
-    console.log(models);
-}
-
-test();
